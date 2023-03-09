1cb2003fb01664f27d6cc9479d79ba9b56a6ab02
hosuappa <92260937+hosuappa@users.noreply.github.com>
2023-03-09 23:42:47 +0900

    라디오 추가

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 3) {
+        basic.showString("3")
+    } else if (receivedNumber == 4) {
+        basic.showString("4")
+    } else {
+        basic.showString("")
+    }
+})
+radio.setGroup(19)
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
     basic.pause(100)
     basic.showIcon(IconNames.SmallHeart)
-    basic.pause(100)
-    basic.showString("Go Seryn !!")
     basic.pause(100)
 })
